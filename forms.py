f54de8af576fc3447dfe8914fcbcc7cbe741607e
Tomasz Luszczak <luszczaktomasz@gmail.com>
2022-06-15 17:56:29 +0200

    Add new files

--- forms.py
+++ forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 class TodoForm(FlaskForm):
     title = StringField('Tytuł', validators=[DataRequired()])
     description = StringField('Opis', validators=[DataRequired()])
-    done = BooleanField('Czy zrobione?')
+    done = BooleanField('Czy zapłacone?')
     
 
--- models.py
+++ models.py
@@ -10,14 +10,23 @@ class Todos:
             self.todos = []
 
     def all(self):
-        return self.todos["todos"]
+        for todo in self.todos:
+            if todo['done'] == False:
+                todo['done'] = "NIE"
+            if todo['done'] == True:
+                todo['done'] = "TAK"
+
+        self.todos = sorted(self.todos, key=lambda k:k['done'])
+
+        return self.todos
 
     def get(self, id):
-        return self.todos["todos"][id]
+        return self.todos[id]
+
 
     def create(self, data):
-        data.pop('csrf_token')
-        self.todos["todos"].append(data)
+        self.todos.append(data)
+        self.save_all()
 
     def save_all(self):
         with open("todos.json", "w") as f:
@@ -25,8 +34,26 @@ class Todos:
 
     def update(self, id, data):
         data.pop('csrf_token')
-        self.todos["todos"][id] = data
+        self.todos[id] = data
         self.save_all()
+
+    def delete(self, id):
+        todo = self.get(id)
+        if todo:
+            self.todos.remove(todo)
+            self.save_all()
+            return True
+        return False
+
+    def update(self, id, data):
+        todo = self.get(id)
+        if todo:
+            index = self.todos.index(todo)
+            self.todos[index] = data
+            self.save_all()
+            return True
+        return False
 
 
 todos = Todos()
+
